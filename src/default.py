f97570f688c689c2db13016fc45de67ec85c6dcf
stefandmn <damian.stefan@gmail.com>
2021-01-03 18:57:11 +0200

    #0000373 using busydialog when the loading time is longer (on any device)

--- src/default.py
+++ src/default.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 
 import sys
-import urllib
 import common
 from searcher import GlobalSearch
 
@@ -14,17 +13,22 @@ else:
 if __name__ == "__main__":
 	searchstring = None
 
-	try:
+	if len(sys.argv) > 1:
 		params = dict(arg.split("=") for arg in sys.argv[1].split("&"))
-		searchstring = params.get("searchstring")
-		searchstring = urllib.unquote_plus(searchstring)
-	except:
+		if searchstring is not None and searchstring != '':
+			searchstring = params.get("searchstring")
+			searchstring = common.urlunquote(searchstring)
+
+	if searchstring is None or searchstring == '':
+		# close busydialog - if is open and running
+		common.runBuiltinCommand("Dialog.Close(busydialog)")
+		# get search string from input
 		keyboard = xbmc.Keyboard('', common.translate(32101), False)
 		keyboard.doModal()
 		if keyboard.isConfirmed():
 			searchstring = keyboard.getText()
 
-	if searchstring:
+	if searchstring is not None and searchstring != '':
 		search = GlobalSearch("GlobalSearch.xml", common.AddonPath(), searchstring=searchstring)
 		search.show()
 		del search
